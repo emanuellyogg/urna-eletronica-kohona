dc3231d0ecb672a56e32cfcf3342c7367341a974
Emanuelly Maria Ogg Vicente <emanuelly.vicente@capgemini.com>
2021-08-12 18:34:42 -0300

    feat: aplicado a funcionalidade de ver o nome e foto dos candidatos

--- js/script.js
+++ js/script.js
@@ -7,10 +7,9 @@ var votoNulo = 0;
 
 function countVotes() {
   let voteSelect = document.querySelector("#idNumCand");
-  
   voteSelect = Number(voteSelect.value);
 
-  switch (voteEnter || voteSelect) {
+  switch (voteSelect) {
     case 11:
       votoCand11++
       break;
@@ -47,6 +46,44 @@ function showResult() {
   console.log({votoNulo});
   
 
+}
+
+function fotoCandidato() {
+  let numCand = document.querySelector("#idNumCand");  
+  numCand = Number(numCand.value);
+  let foto;
+  let candidato;
+
+  switch (numCand) {
+    case 11:
+      foto = "jiraiya.jpg";
+      candidato = "Jiraiya Sensei"
+      break;
+    case 12:
+      foto = "kakashi.jpg";
+      candidato = "Kakashi Hatake"
+      break;
+    case 13:
+      foto = "shikamaru.jpg";
+      candidato = "Shikamaru Nara"
+      break;      
+    default:
+      if (numCand == "") {
+        foto = "branco.png";
+      candidato = "Voto em Branco"
+      } else {
+        foto = "nulo.png";
+      candidato = "Voto Nulo"
+      }
+      break;
+  }
+
+
+  let img = document.querySelector("#imgCandidate");
+  img.src = "images/" + foto;
+
+  let nomeCand = document.querySelector("#NmCandidato");
+  nomeCand.value = candidato;
 }
 
 // criar função limpar a tela - será chamado quando confirmar voto, voto em branco e btn-reset;
